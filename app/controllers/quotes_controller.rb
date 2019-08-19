cf4d6950872732cd62106300cefae376310609fd
Amy Donnelly <akdonnelly@gmail.com>
2019-08-19 05:17:31 +0000

    valid data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,13 +8,17 @@ class QuotesController < ApplicationController
     end
 
     def create
-      Quote.create(quote_params)
+      @quote = Quote.create(quote_params)
+      if @quote.invalid?
+        flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+      end
       redirect_to root_path
     end
 
+
     private
 
-    def quote_params
-      params.require(:quote).permit(:saying, :author)
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
   end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 140, mininum: 3 }
+  validates :author, presence: true, length: { maximum: 50, mininum: 3 }
 end
